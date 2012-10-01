e1837007d32d5498d069c498f34eb98dd29ab3c2
Edvard Pedersen <epe005@epe005-skole.(none)>
2012-10-01 19:00:08 +0200

    Fixing GeStore calls, one at a time

--- scripts/startGePan.pl
+++ scripts/startGePan.pl
@@ -833,8 +833,11 @@ sub _printSchedulerShell{
                                 'format'=>"fasta" } );
         print IN "done\n";
 
-        print IN "hadoop jar ".GESTORE_PATH." org.diffdb.move -D file=".$params->{'script_id'}."protein_exporter.fas -D run=".$params->{'script_id'}." -D type=r2l -conf=".GESTORE_CONFIG."\n";
-        print IN "mv ".$params->{'script_id'}."protein_exporter.fas exporter.fas\n";
+        #print IN "hadoop jar ".GESTORE_PATH." org.diffdb.move -D file=".$params->{'script_id'}."protein_exporter.fas -D run=".$params->{'script_id'}." -D type=r2l -conf=".GESTORE_CONFIG."\n";
+        _printGeStoreCall(*IN, {'filename'=>"protein_exporter.fas",
+				  'run'=>$params->{'script_id'},
+				  'type'=>"r2l"});
+        print IN "mv protein_exporter.fas exporter.fas\n";
         print IN "mv exporter.fas ".NODE_LOCAL_PATH."/gepan/".'$JOB_ID/input/'."\n";
         print IN "rm ".NODE_LOCAL_PATH."/gepan/".'$JOB_ID/output/*'."\n";
     } else {
